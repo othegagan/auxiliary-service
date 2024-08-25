1c868bae514b4f3f8dfb5ec9bbfd0fad7434db4f
Gagan Kumar <gagan.kumar@dimension64.com>
2024-08-25 11:33:50 +0530

    fix format

--- src/controllers/chat/createBooking.controller.ts
+++ src/controllers/chat/createBooking.controller.ts
@@ -1,8 +1,8 @@
-import { env } from "@/configs/env";
-import logger from "@/utils/logger";
-import axios from "axios";
-import logUpdate from "@/controllers/logUpdate.controller";
-import approveBooking from "@/controllers/booking/approveBooking.controller";
+import { env } from '@/configs/env';
+import approveBooking from '@/controllers/booking/approveBooking.controller';
+import logUpdate from '@/controllers/logUpdate.controller';
+import logger from '@/utils/logger';
+import axios from 'axios';
 
 export default async function createBooking(startDate: string, endDate: string, vehicleId: number) {
     let days = (new Date(startDate).valueOf() - new Date(endDate).valueOf()) / (1000 * 60 * 60 * 24);
@@ -12,41 +12,41 @@ export default async function createBooking(startDate: string, endDate: string, 
 
     const payload = {
         vehicleid: vehicleId,
-        userId: "1378",
+        userId: '1378',
         startTime: startDate,
         endTime: endDate,
-        channelName: "turo",
+        channelName: 'turo',
         deductionfrequencyconfigid: 1,
         paymentauthorizationconfigid: 1,
         authorizationamount: 1,
         authorizationpercentage: 1,
         perDayAmount: 1,
         totalDays: `${days}`,
-        stripePaymentToken: "pi_3O2SWMAHBUVqiOLM2UEfeLIF",
-        stripePaymentID: "NA",
+        stripePaymentToken: 'pi_3O2SWMAHBUVqiOLM2UEfeLIF',
+        stripePaymentID: 'NA',
         stripePaymentTransactionDetail: '{ "key1" : "val1" }',
-        paymentMethodIDToken: "NA",
-        customerToken: "cus_OpmOXorq5jctzC",
-        setupIntentToken: "NA",
-        isCustomerTokenNew: "NA",
-        comments: "Request to book",
+        paymentMethodIDToken: 'NA',
+        customerToken: 'cus_OpmOXorq5jctzC',
+        setupIntentToken: 'NA',
+        isCustomerTokenNew: 'NA',
+        comments: 'Request to book',
         taxAmount: 1,
         tripTaxAmount: 1,
         totalamount: 1,
-        tripamount: "1",
+        tripamount: '1',
         pickupTime: startDate,
-        dropTime: endDate,
+        dropTime: endDate
     };
 
     const headers = {
-        "Content-Type": "application/json",
-        Bundee_auth_token: env.BUNDEE_AUTH_TOKEN,
+        'Content-Type': 'application/json',
+        Bundee_auth_token: env.BUNDEE_AUTH_TOKEN
     };
 
     try {
         const response = await axios.post(url, payload, { headers: headers });
         logger.info(response.data);
-        if (response.data.errorCode === "0") {
+        if (response.data.errorCode === '0') {
             const bookingId = response.data.activeTrips[0].tripid;
             await approveBooking(bookingId);
             await logUpdate(response.data.errorMessage);
