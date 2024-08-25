4aa1db954a17fd0581797abdee18adf57f6acb08
othegagan <gagankumar8711@gmail.com>
2024-08-25 20:20:26 +0530

    Update config for vercel

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import app from '@/app';
 import { env } from '@/configs/env';
 import logger from '@/utils/logger';
 
-const PORT = env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     logger.info(`Server is running in ${env.NODE_ENV} mode on port ${PORT}`);
